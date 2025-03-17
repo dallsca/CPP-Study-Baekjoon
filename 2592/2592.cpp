e9152ecf5d8b3cf762232ffcd2eeb495f70a8e4b
dallsca <dallsca@naver.com>
2025-03-17 16:04:07 +0900

    modify 2592

--- 2592/2592.cpp
+++ 2592/2592.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main()
-{
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		int typo;
-		std::string name;
-		cin >> typo >> name;
-		for (int j = 0; j < name.length(); j++)
-		{
-			
-			if (j != typo-1)
-			{
-				cout << name[j];
-			}
-			
-		}
-		cout << endl;
-	}
+int main() {
+    int values[10];
+    int frequency[1001] = {0}; 
+    int sum = 0;
+    int maxIndex = 0; 
+
+    for (int i = 0; i < 10; i++) {
+        cin >> values[i];
+        sum += values[i];
+        frequency[values[i]]++;
+    }
+
+    int maxFrequency = 0; 
+    for (int i = 0; i < 1001; i++) {
+        if (frequency[i] > maxFrequency) {
+            maxFrequency = frequency[i];
+            maxIndex = i;
+        }
+    }
+
+    cout << sum / 10 << endl; 
+    cout << maxIndex << endl; 
 }
