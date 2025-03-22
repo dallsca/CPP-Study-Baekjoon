8bc244d450f478f33b8fd37c579a0983e693f0a5
dallsca <dallsca@naver.com>
2025-03-22 20:46:04 +0900

    Add solution for Baekjoon problem 2750

--- 5054/5054.cpp
+++ 5054/5054.cpp
@@ -1,32 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int T;
-    cin >> T;
-    for (int i = 0; i < T; i++) {
-        int n;
-        cin >> n;
-        int* a = new int[n];
-        for (int j = 0; j < n; j++) {
-            cin >> a[j];
-        }
-
-        int min = 0, max = 0;
-        for (int j = 0; j < n; j++) {
-            if (a[j] < a[min]) {
-                min = j;
-            }
-            if (a[j] > a[max]) {
-                max = j;
-            }
-        }
-
-        int minlength = (a[max] - a[min]) * 2;
-        delete[] a;
-        cout << minlength << endl;
-    }
-    return 0;
+int main()
+{
+	int maxnum[5];
+	int arr[8];
+	for (int i = 0; i < 8; i++)
+	{
+		cin >> arr[i];
+	}
+	for (int i = 0; i < 8; i++)
+	{
+		cout << arr[i];
+	}
+	return 0;
 }
-
-//제일 작은 값과 제일 큰 값을 찾아서 그 두개의 차이에 2배를 하는것이 최소한의 거리이다.
